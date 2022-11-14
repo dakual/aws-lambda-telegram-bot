3e0113de147b2dfb2aca6082666cab7797eb6fe7
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2022-11-14 17:09:27 +0300

    first commit

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ variable "invocation" {
 
 
 resource "aws_cloudwatch_log_group" "api" {
-  name              = "lambda/${aws_lambda_function.main.function_name}"
+  name              = "/aws/lambda/${aws_lambda_function.main.function_name}"
   retention_in_days = 14
 }
 
@@ -131,23 +131,23 @@ resource "aws_apigatewayv2_stage" "main" {
   name        = "$default"
   auto_deploy = true
 
-  # access_log_settings {
-  #   destination_arn = aws_cloudwatch_log_group.api.arn
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api.arn
 
-  #   format = jsonencode({
-  #     requestId               = "$context.requestId"
-  #     sourceIp                = "$context.identity.sourceIp"
-  #     requestTime             = "$context.requestTime"
-  #     protocol                = "$context.protocol"
-  #     httpMethod              = "$context.httpMethod"
-  #     resourcePath            = "$context.resourcePath"
-  #     routeKey                = "$context.routeKey"
-  #     status                  = "$context.status"
-  #     responseLength          = "$context.responseLength"
-  #     integrationErrorMessage = "$context.integrationErrorMessage"
-  #     }
-  #   )
-  # }
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
 }
 
 resource "aws_apigatewayv2_integration" "main" {
